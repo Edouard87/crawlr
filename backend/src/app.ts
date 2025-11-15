e31fd465eff93eb6d320f8bffd22d0184cb9e0c6
Edouard87 <edouard.desparois-perrault@mail.mcgill.ca>
2025-11-15 06:56:59 -0500

    db config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import routes from './routes'
+import mongoose from 'mongoose'
 
 dotenv.config();
 
 const app: Application = express();
+
+mongoose.connect(process.env.MONGO_URI)
 
 app.use(express.json());
 
